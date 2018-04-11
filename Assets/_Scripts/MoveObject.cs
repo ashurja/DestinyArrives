7cef859afcd31bb1ea6f11968afd10d1fed767b1
Jamshed Ashurov <jamshedashurov@Jamsheds-MacBook-Pro.local>
2018-04-11 14:33:13 -0400

    First try

---
+++ Assets/_Scripts/MoveObject.cs
@@ -0,0 +1,21 @@
+﻿// Jamshed Ashurov 
+// 04/11/2018
+// This script is used to move the objects(ship for now).
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent <Rigidbody> ().velocity = new Vector3 (-4, 0, 0);
+		// Moves the object on the x axis. 
+	}
+}
